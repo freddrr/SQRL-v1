bd0e76788fe74fabfc27c386f67fd6300a7dbd97
Freddy Trae Torres <ftorres0448@gmail.com>
2024-09-06 17:46:48 -0500

    Added Login Info post call to server, only sends email currently

--- SQRL/server/server.js
+++ SQRL/server/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const corsOptions = {
   origin: ["http://localhost:5173"],
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/api", (req, res) => {
+    console.log("GET request recieved from client");
     res.json({fruits: ["apple", "orange", "banana"]});
+});
 
+app.post("/loginInfo", (req, res) => {
+    console.log("POST request recieved from client");
+    console.log("User email is: " + req.body.email);
+    res.json({returnString: "Email received"});
 });
 
 app.listen(8080, () => {
